a1451c415840e3b00619364da824930a128edf4c
chrv <c.h.onguene@hotmail.com>
2021-02-18 22:48:00 +0100

    Update python 2 to 3

--- topics.py
+++ topics.py
@@ -12,27 +12,27 @@ def print_topics(beta_file, vocab_file, nwords = 25):
 
     # get the vocabulary
 
-    vocab = file(vocab_file, 'r').readlines()
+    vocab = open(vocab_file, 'r').readlines()
     # vocab = map(lambda x: x.split()[0], vocab)
-    vocab = map(lambda x: x.strip(), vocab)
+    vocab = [x.strip() for x in vocab]
 
     # for each line in the beta file
 
-    indices = range(len(vocab))
+    indices = list(range(len(vocab)))
     topic_no = 0
-    for topic in file(beta_file, 'r'):
-        print 'topic %03d' % topic_no
-        topic = map(float, topic.split())
-        indices.sort(lambda x,y: -cmp(topic[x], topic[y]))
+    for topic in open(beta_file, 'r'):
+        print('topic %03d' % topic_no)
+        topic = list(map(float, topic.split()))
+        indices.sort(key=lambda x: -topic[x])
         for i in range(nwords):
-            print '   %s' % vocab[indices[i]]
+            print('   {:15} : {}'.format(vocab[indices[i]], topic[indices[i]]))
         topic_no = topic_no + 1
-        print '\n'
+        print('\n')
 
 if (__name__ == '__main__'):
 
     if (len(sys.argv) != 4):
-       print 'usage: python topics.py <beta-file> <vocab-file> <num words>\n'
+       print('usage: python topics.py <beta-file> <vocab-file> <num words>\n')
        sys.exit(1)
 
     beta_file = sys.argv[1]
